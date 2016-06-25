74b4f08a67633a93a3594419ef35187274022865
Oliver Paz <oliver_paz@fisica.unam.mx>
2016-06-24 19:15:46 -0500

    new plot oliver

--- graficaNormal.R
+++ graficaNormal.R
@@ -1,10 +1,7 @@
 a <- rnorm(1000)
 plot(a)
-<<<<<<< HEAD
-library("ggplot2")
-qplot(a)
-=======
 
-m <- rnorm(1000,0,1, main = "Mario")
-plot(m)
->>>>>>> f464eb385aeb09b6b02dfef7904e9d1e288f2f39
+
+
+b <- rnorm(1000, 0,1)
+plot(b)
